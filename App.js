afd5207e8d9feaf7b68892604e4fa1a5cdf52b8f
MarcioRMed <contato.microinfo@gmail.com>
2022-06-23 07:32:41 -0300

    ajustes

--- App.js
+++ App.js
@@ -1,53 +1,56 @@
-import React from 'react';
-import {StyleSheet , View} from 'react-native';
+// import React from 'react';
+import {StyleSheet} from 'react-native';
 import TelaJogo from './components/telaJogo'
 import TelaConfig from './components/telaConfig'
-import {NavigationContainer} from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
-const Menus=createBottomTabNavigator()
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App(){
-  return(
-    <View>
-    <TelaJogo/>
-    <TelaConfig/>
-    </View>)}
 
-//       <NavigationContainer theme={temaMenu}>
-//         <Menus.Navigator initialRouteName='Jogo'>
-        
-//         <Menus.Screen
-//           // style={styles.menu}
-//           name="Jogo"
-//           component={TelaJogo}
-//           options={{
-//             title:'Jogo',
-//           }}
-//         />
-        
-//         <Menus.Screen 
-//           nome="Configurações"
-//           component={TelaConfig}
-//          />
-        
-//         </Menus.Navigator>
-//       </NavigationContainer>
-//   )
-// }
+function HomeScreen() {
+  return (
+    <View >
+     <TelaJogo/>
+    </View>
+  );
+}
 
-// const temaMenu={
-//   dark:false,
-//   colors:{
-//     primary: 'rgb(255,255,255)', //cor do texto menu selecionado
-//     background: 'rgb(0,0,0)', //cor de fundo
-//     card: 'rgb(0,0,0)', //cor do fundo do menu
-//     text: 'rgb(255,255,255)', //cor texto
-//     border: 'rgb(255,255,255)', //cor da borda
-//     notification: 'rgb(255,0,0)', //cor do emblema do navegador
+function SettingsScreen() {
+  return (
+    <View >
+      <TelaConfig/>
+    </View>
+  );
+}
 
-//   }
-// }
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer theme={temaMenu}>
+      <Tab.Navigator>
+        <Tab.Screen name="Jogo" component={HomeScreen} />
+        <Tab.Screen name="Configurações" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+ const temaMenu={
+   dark:false,
+   colors:{
+     primary: 'rgb(255,255,255)', //cor do texto menu selecionado
+     background: 'rgb(0,0,0)', //cor de fundo
+     card: 'rgb(0,0,0)', //cor do fundo do menu
+     text: 'rgb(255,255,255)', //cor texto
+     border: 'rgb(255,255,255)', //cor da borda
+     notification: 'rgb(255,0,0)', //cor do emblema do navegador
+
+   }
+ }
 
 
 const styles = StyleSheet.create({
